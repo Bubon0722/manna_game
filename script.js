9ce5a52d37ded36955aa9bec4e3c60420fdfe432
Bubon0722 <37260749+Bubon0722@users.noreply.github.com>
2024-11-16 01:58:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const dialogues = [
-  { text: "這是第一則對話，白色文字逐字彈出。", image: "image1.jpg" },
-  { text: "接著是第二則對話，同樣逐字顯示。", image: "image2.jpg" },
-  { text: "最後一則對話，按一下跳轉至指定頁面。", image: "image3.jpg" }
+  { text: "今天是2024年12月31日，禮拜二。。", image: "image1.jpg" },
+  { text: "中午，我一如往常地來到哎哎咖啡打工，協助點餐、做餐。", image: "image1.jpg" },
+  { text: "「哈囉！」我推開門，熱情地打招呼－－", image: "image1.jpg" },
+  { text: "空無一人。", image: "image2.jpg" },
+  { text: "連照理來說這時候會出現的弘麒哥也不在。", image: "image2.jpg" },
+  { text: "「好吧。」見沒人回應，我也只能站上崗位，開始備料、等待客人上門。", image: "image2.jpg" },
+  { text: "但過了一個小時...", image: "image2.jpg" },
+  { text: "仍然一個人都沒有。", image: "image2.jpg" },
+  { text: "沒有客人、沒有伯宜哥、沒有團契的弟兄姊妹們。", image: "image2.jpg" },
+  { text: "我心想，大家是都去跨年了嗎？", image: "image2.jpg" },
+  { text: "「算了，等等還有那超鋼的財務管理，我先去上課好了。」", image: "image2.jpg" },
+  { text: "「靠杯，這門怎麼突然鎖住了，還轉不開！」", image: "image3.jpg" },
+  { text: "「而且密碼鎖什麼時候反過來裝了？？」", image: "image4.jpg" },
+  { text: "我拿起手機，準備打電話給伯宜哥求救，結果發現......", image: "image2.jpg" },
+  { text: "手！機！沒！電！了！", image: "image2.jpg" }
 ];
 
-const redirectUrl = "https://example.com"; // 替換為您的目標連結
+const redirectUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdn7J7h8SNBwxiDdgn0JA7D3mm_s6HO99qXAZHqskNrVGaryQ/viewform"; // 替換為您的目標連結
 
 let currentDialogueIndex = 0;
 const textElement = document.getElementById('dialogue-text');
